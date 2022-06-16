a3d3e1241cd0c66055b13fe0739fe5cbffdd583d
pandoraborealis <callmevolk@gmail.com>
2022-06-16 14:23:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//---------- Navbar ----------//
+
+window.addEventListener('scroll', function(){
+  let navbar = document.getElementById("navbar");
+  navbar.classList.toggle('fixed', this.window.scrollY > 0)
+})
